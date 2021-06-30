42da5fe9efbf288f944800b97732500f5c9a744b
dev <devyadav5392@gmail.com>
2021-06-30 10:19:25 +0200

    random_number_and_random_int

--- random_number.py
+++ random_number.py
@@ -3,3 +3,7 @@ def random_number():
     #a=list(range(1,101))       #create the list of range 1 to 100
     result=random.choice(list(range(1,101)))
     return result
+
+def random_int():
+    import random
+    return random.randint(1,100)
