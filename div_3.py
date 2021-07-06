75702ef7d5884d4d540c666c6625731b991b269e
dev <devyadav5392@gmail.com>
2021-07-06 11:10:15 +0200

    div_3

---
+++ div_3.py
@@ -0,0 +1,12 @@
+#This function returns True if its single integer parameter is divisible by
+#3 and False otherwise
+
+def div_3(x):
+    if x%3==0:
+        result=True
+    else:
+        result=False
+    return result
+
+''' def div_3(x):
+        return x%3 == 0'''
