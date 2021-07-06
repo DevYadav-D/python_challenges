94a36f82e9cf98aa7ec379c27980301cc48266b4
dev <devyadav5392@gmail.com>
2021-07-06 11:02:23 +0200

    largest_difference

---
+++ largest_difference.py
@@ -0,0 +1,6 @@
+#This function should compute and return the difference
+#between the largest and smallest number in the list
+
+def largest_difference(x):
+    result = max(x)-min(x)
+    return result
