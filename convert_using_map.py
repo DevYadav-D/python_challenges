e5b4e16fa4695ad13a9a851acf168d31e9469e4b
dev <devyadav5392@gmail.com>
2021-07-07 12:52:46 +0200

    basic_example_of_map

---
+++ convert_using_map.py
@@ -0,0 +1,6 @@
+#This function takes a list of numbers as its only parameters and
+#returns a list of each number converted to a string
+# *** using of map is important
+
+def convert(l):
+    return list(map(str,l))
